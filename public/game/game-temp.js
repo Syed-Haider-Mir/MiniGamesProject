942d931ab2eec5e1e20a1673520e54ac5e8f0452
Syed Haider Mir <haidersyed.mir@gmail.com>
2023-11-02 01:01:56 -0400

    Added to the game-temp file

--- public/game/game-temp.js
+++ public/game/game-temp.js
@@ -15,3 +15,33 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+let msg1 = document.getElementById('message1');
+let msg2 = document.getElementById('message2');
+let msg3 = document.getElementById('message3');
+
+var answer = Math.floor(Math.random() * 100) + 1;
+var no_of_guesses = 0;
+var guessed_nums = [];
+
+function play() {
+    var user_guess = document.getElementById('guess').value;
+
+    if (user_guess < 1 || user_guess > 100) {
+        alert('Please choose another number between 1 to 100');
+    } else {
+        guessed_nums.push(user_guess);
+        no_of_guesses += 1;
+
+        if (user_guess < answer) {
+            msg1.textContent = 'Your guess is too low';
+        } else if (user_guess > answer) {
+            msg1.textContent = 'Your guess is too high';
+        } else if (user_guess == answer) {
+            msg1.textContent = 'Congrats, you have won!';
+            document.getElementById("my_btn").disabled = true;
+        }
+
+        msg2.textContent = 'No. of guesses: ' + no_of_guesses;
+        msg3.textContent = 'Guessed numbers are: ' + guessed_nums;
+    }
+}
