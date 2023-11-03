f6b1acbfacc99331f8d1c946a4b0ce583e053245
Syed Haider Mir <haidersyed.mir@gmail.com>
2023-11-02 22:46:13 -0400

    Added new modification to my html and css template

--- public/Haider_game/script11.js
+++ public/Haider_game/script11.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const game = document.querySelector("game");
+    const gameDropdownMenu = document.querySelector(".game-dropdown");
+
+    // Show the dropdown menu when hovering over the "Games" link
+    game.addEventListener("mouseenter", function() {
+        gameDropdownMenu.style.display = "flex";
+    });
+
+    // Hide the dropdown menu when clicking outside of it
+    game.addEventListener("mouseleave", function(event) {
+        if (!game.contains(event.relatedTarget) && !gameDropdownMenu.contains(event.relatedTarget)) {
+            gameDropdownMenu.style.display = "none";
+        }
+    });
+});
+
 let msg1 = document.getElementById('message1');
 let msg2 = document.getElementById('message2');
 let msg3 = document.getElementById('message3');
